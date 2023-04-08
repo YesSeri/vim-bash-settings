8ccf235d41c0a30885c1e3a6fa43466d3133e9a2
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-04-08 21:28:29 +0200

    removed hist

--- linux/cron-backup-script.sh
+++ linux/cron-backup-script.sh
@@ -5,8 +5,9 @@ cp /home/henrik/.bashrc					/home/henrik/programming/docs/vim-bash-settings/linu
 cp /home/henrik/.fzf.bash 			   	/home/henrik/programming/docs/vim-bash-settings/linux/config/fzf.bash
 cp /home/henrik/.bash_aliases  		   	/home/henrik/programming/docs/vim-bash-settings/linux/config/bash_aliases
 cp /home/henrik/.config/nvim/init.vim  	/home/henrik/programming/docs/vim-bash-settings/linux/config/nvim/init.vim
+cp -r /home/henrik/.doom.d 				/home/henrik/programming/docs/vim-bash-settings/linux/config/doom.d
 
-git add /home/henrik/programming/docs/vim-bash-settings/linux/config/bat/config /home/henrik/programming/docs/vim-bash-settings/linux/config/bashrc /home/henrik/programming/docs/vim-bash-settings/linux/config/fzf.bash /home/henrik/programming/docs/vim-bash-settings/linux/config/bash_aliases /home/henrik/programming/docs/vim-bash-settings/linux/config/nvim/init.vim
+git add /home/henrik/programming/docs/vim-bash-settings/linux/config/bat/config /home/henrik/programming/docs/vim-bash-settings/linux/config/bashrc /home/henrik/programming/docs/vim-bash-settings/linux/config/fzf.bash /home/henrik/programming/docs/vim-bash-settings/linux/config/bash_aliases /home/henrik/programming/docs/vim-bash-settings/linux/config/nvim/init.vim /home/henrik/programming/docs/vim-bash-settings/linux/config/doom.d
 
 git commit -m "Backup: $(date)"
 

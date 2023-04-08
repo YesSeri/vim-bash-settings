6bd85d17e0bf38efcc87ec4975692ff151c57408
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-04-08 21:16:33 +0200

    added cron script

---
+++ linux/cron-backup-script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cp /home/henrik/.config/bat/config 		/home/henrik/programming/docs/vim-bash-settings/linux/config/bat/config
+cp /home/henrik/.bashrc					/home/henrik/programming/docs/vim-bash-settings/linux/config/bashrc
+cp /home/henrik/.fzf.bash 			   	/home/henrik/programming/docs/vim-bash-settings/linux/config/fzf.bash
+cp /home/henrik/.bash_aliases  		   	/home/henrik/programming/docs/vim-bash-settings/linux/config/bash_aliases
+cp /home/henrik/.config/nvim/init.vim  	/home/henrik/programming/docs/vim-bash-settings/linux/config/nvim/init.vim
+
+git add /home/henrik/programming/docs/vim-bash-settings/linux/config/bat/config /home/henrik/programming/docs/vim-bash-settings/linux/config/bashrc /home/henrik/programming/docs/vim-bash-settings/linux/config/fzf.bash /home/henrik/programming/docs/vim-bash-settings/linux/config/bash_aliases /home/henrik/programming/docs/vim-bash-settings/linux/config/nvim/init.vim
+
+git commit -m "backup $(date)"
+
+git push origin main
+
+echo "$(date) : backup to git"

3188bb2a78bd8a0a352cc4e63dbe10dda4ce4373
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-04-08 21:26:40 +0200

    Backup: Sat Apr  8 09:26:40 PM CEST 2023

---
+++ linux/config/doom.d/.doom.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(org-agenda-files
+   '("~/Dropbox/orgzly/todo.org" "/home/henrik/Dropbox/orgzly/Documentation.org" "/home/henrik/Documents/tankar/tankar.org" "/home/henrik/programming/web/github-blogg/yesseri.github.io/plans.org" "/home/henrik/org/notes.org" "/home/henrik/org/todo.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'dap-node)

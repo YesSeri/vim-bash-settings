b1b69f2adb2613b1bd6cee16e129ec55a10ebabb
henrik <henrik.zenkert@gmail.com>
2023-08-18 13:17:41 +0200

    added nvim config

--- linux/config/nvim/nvim/lua/keymap/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader=','
-vim.keymap.set('n', '<leader>t', 'a<C-R>=strftime("%c")<CR><Esc>' )
-vim.keymap.set('n', '<leader>n', ':bnext<CR>' )
-vim.keymap.set('n', '<leader>b', ':bprevious<CR>' )
-
-
-
-
-
--- linux/cron-backup-script.sh
+++ linux/cron-backup-script.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-cp /home/henrik/.config/bat/config 		/home/henrik/programming/docs/vim-bash-settings/linux/config/bat/config
 cp /home/henrik/.bashrc					/home/henrik/programming/docs/vim-bash-settings/linux/config/bashrc
 cp /home/henrik/.fzf.bash 			   	/home/henrik/programming/docs/vim-bash-settings/linux/config/fzf.bash
 cp /home/henrik/.bash_aliases  		   	/home/henrik/programming/docs/vim-bash-settings/linux/config/bash_aliases
 cp -r /home/henrik/.config/nvim		 	/home/henrik/programming/docs/vim-bash-settings/linux/config/nvim
-cp -r /home/henrik/.doom.d 				/home/henrik/programming/docs/vim-bash-settings/linux/config/doom.d
 
 
 if [[ `git status --porcelain` ]]; then

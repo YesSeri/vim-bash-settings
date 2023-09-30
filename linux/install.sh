03537a90116856b1fd1ff1d5e0c61e9ca506bb3c
Henrik <henrik.zenkert@gmail.com>
2023-09-30 21:20:42 +0200

    install lazygit improved

--- linux/install.sh
+++ linux/install.sh
@@ -44,6 +44,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 # lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
+curl -Lo ~/Downloads/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf ~/Downloads/lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm lazygit

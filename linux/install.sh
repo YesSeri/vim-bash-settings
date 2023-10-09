9f6ef9269c07b603c4313cbdd248d828787dad18
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-10-09 10:05:55 +0200

    delete unneccesary

--- linux/install.sh
+++ linux/install.sh
@@ -30,3 +30,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ./subscripts/lazygit.sh
+$(./subscripts/dropbox.sh)

0d93560563a797d08f398d0a24b357b92f6ed631
Henrik <henrik.zenkert@gmail.com>
2024-03-07 11:49:42 +0100

    added tig and fixed copy

--- copy_config_to_repo.sh
+++ copy_config_to_repo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cp ~/.bashrc ~/.bash_aliases ~/.fzf.bash ./config
-cp -r ~/.config/bat/ ~/.config/i3/ ~/.config/kitty/ ~/.config/nvim/ 
+cp -r ~/.config/bat/ ~/.config/i3/ ~/.config/kitty/ ~/.config/nvim/ ~/.config/tig/ ./config/.config

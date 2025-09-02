48f1262eb5b264be96828bcfd8ec17df21b1c84b
exheze <exheze@dbc.dk>
2025-09-02 21:44:44 +0200

    update

--- nixos/homeserver/flake.nix
+++ nixos/homeserver/flake.nix
@@ -1,18 +1,26 @@
 {
   description = "NixOS for henrikserver-slow-nixos";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  inputs.hostmap.url = "github:YesSeri/hostmap";
+  inputs.hostmap.url = "github:YesSeri/hostmap?ref=minimal-counter";
 
 
-  outputs = { self, nixpkgs, hostmap }: {
+  outputs = { self, nixpkgs, hostmap, ... }: {
     nixosConfigurations.henrikserver-slow-nixos =
       nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hardware-configuration.nix
           ./configuration.nix
-	  { nixpkgs.overlays = [ (self: super: { hostmap = hostmap.packages.x86_64-linux.hostmap; }) ]; }
+	  { 
+	  nixpkgs.overlays = [
+            (final: prev: {
+              hostmap = hostmap.defaultPackage.${final.system or "x86_64-linux"};
+            })
+          ];
+        }
+
         ];
+
       };
   };
 }
